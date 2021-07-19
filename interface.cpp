ee3e288bab7be369b145960425c3d16105508439
robbey@deepsight.ai <robbey@deepsight.ai>
2021-07-19 16:38:12 +0800

    #update: trival

--- interface.cpp
+++ interface.cpp
@@ -1,6 +1,17 @@
 #include "rea.h"
+#include "reaJS.h"
 #include <QQmlApplicationEngine>
 #include "mainwindow.h"
+
+static rea::regPip<QQmlApplicationEngine*> reg_js_linker([](rea::stream<QQmlApplicationEngine*>* aInput){
+    qmlRegisterType<rea::environmentJS>("EnvJS", 1, 0, "EnvJS");
+    //ref from: https://stackoverflow.com/questions/25403363/how-to-implement-a-singleton-provider-for-qmlregistersingletontype
+    rea::pipeline::instance()->updateOutsideRanges({"js"});
+    rea::pipeline::instance("qml")->updateOutsideRanges({"qmljs"});
+    rea::pipeline::instance("js");
+    rea::pipeline::instance("qmljs");
+    aInput->out();
+}, rea::Json("name", "install0_js"), "initRea");
 
 QImage Uri2QImage(const QString& aUri){
     auto dt = aUri.mid(aUri.indexOf("64,") + 3, aUri.length()).trimmed();
