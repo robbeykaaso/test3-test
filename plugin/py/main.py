1f45c8a35f69574a40908ca7efecd991507be7c9
robbey@deepsight.ai <robbey@deepsight.ai>
2021-08-03 15:53:13 +0800

    #update: trival

--- plugin/py/main.py
+++ plugin/py/main.py
@@ -1,12 +1,40 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
-from server import normalServer
-import test
+from reapython.server import normalServer
+from reapython.rea import pipelines, pipeline, stream
+from reapython.reaRemote import connectRemote, pipelineRemote
+#import test
+
+pipelines().add(lambda aInput:
+    aInput.setData(pipeline("server")).out()
+, {"name": "createserverpipeline"})
+
+pipelines().add(lambda aInput:
+    aInput.setData(pipelineRemote("c++", "server")).out()
+, {"name": "createc++pipeline"})
+
+pipelines().add(lambda aInput:
+    aInput.setData(pipelineRemote("qml", "server")).out()
+, {"name": "createqmlpipeline"})
+
+pipelines("server").add(lambda aInput: 
+    aInput.out()
+, {"name": "logTrain",
+   "external": "qml"})
+
+def clientOnline(aInput: stream):
+    aInput.outs("hello", "logTrain")
+pipelines("server").add(clientOnline, {"name": "clientOnline", "external": "c++"})
 
 def main():
 
     app = QApplication(sys.argv)
+
     server = normalServer()
+    def writeRemote(aInput: stream):
+        server.writeSocket(aInput.scope().data("socket"), aInput.data())
+    connectRemote("server", "c++", writeRemote, False, "c++_server")
+    connectRemote("server", "qml", writeRemote, False, "qml_server")
     #test.doTest(1)
 
     # w = QWidget()
